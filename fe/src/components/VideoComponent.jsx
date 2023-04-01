62df39abe56561d39f837260ee594761560f06df
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-04-01 23:05:01 +0300

    player implemented

--- fe/src/components/VideoComponent.jsx
+++ fe/src/components/VideoComponent.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Youtube from "react-youtube";
 
-const VideoComponent = () => {
+const VideoComponent = (props) => {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const { url } = props;
+  const [playerState, setPlayerState] = useState();
+
+  const onReady = (event) => {
+    // access to player in all event handlers via event.target
+    setPlayer(event.target);
+    event.target.pauseVideo();
+  };
+
+  const onStateChange = (e) => {
+    setPlayerState(e);
+    console.log("onStateChange", e);
+    console.log("videoTitle", e.target.videoTitle);
+    console.log("playerInf", e.target.playerInfo);
+  };
+
+  const onPlaybackRateChange = (event) => {
+    //console.log("onPlaybackRateChange", event);
+  };
+
   return (
-    <div>VideoComponent</div>
-  )
-}
+    <div>
+      <h3>{`currentTime: ${playerState?.target?.playerInfo.currentTime}`}</h3>
+      <h3>{`playbackRate: ${playerState?.target?.playerInfo.playbackRate}`}</h3>
+      <h3>{`videoUrl: ${playerState?.target?.playerInfo.videoUrl}`}</h3>
+      <h3>{`volume: ${playerState?.target?.playerInfo.volume}`}</h3>
+      <h3>{`playerState: ${playerState?.target?.playerInfo.playerState}`}</h3>
+      <Youtube
+        videoId={youtube_parser(url)}
+        opts={opts}
+        onStateChange={onStateChange}
+        onPlaybackRateChange={onPlaybackRateChange}
+        onReady={onReady}
+      />
+    </div>
+  );
+};
 
-export default VideoComponent
+export default VideoComponent;
