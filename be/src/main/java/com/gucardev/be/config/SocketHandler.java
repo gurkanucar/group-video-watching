40b66d56a4988256c27354d72f6cb71bd31689ab
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-04-01 23:18:47 +0300

    onConnect and onDisconnect handlers implemented

--- be/src/main/java/com/gucardev/be/config/SocketHandler.java
+++ be/src/main/java/com/gucardev/be/config/SocketHandler.java
@@ -1,6 +1,11 @@
 package com.gucardev.be.config;
 
+import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.OnConnect;
+import com.corundumstudio.socketio.annotation.OnDisconnect;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +14,24 @@ import org.springframework.stereotype.Component;
 public class SocketHandler {
 
   private final SocketIOServer server;
+  private static final Map<String, String> users = new HashMap<>();
 
   public SocketHandler(SocketIOServer server) {
     this.server = server;
     server.addListeners(this);
     server.start();
+  }
+
+  @OnConnect
+  public void onConnect(SocketIOClient client) {
+    String clientId = client.getSessionId().toString();
+    log.info("Client connected: {}", clientId);
+    users.put(clientId, null);
+  }
+
+  @OnDisconnect
+  public void onDisconnect(SocketIOClient client) {
+    String clientId = client.getSessionId().toString();
+    log.info("Client disconnected: {} ", clientId);
   }
 }
