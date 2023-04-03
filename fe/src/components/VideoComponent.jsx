428cdbd505cc48641270bca7c3dba2c2e767bec6
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-04-03 15:33:50 +0300

    video component updated

--- fe/src/components/VideoComponent.jsx
+++ fe/src/components/VideoComponent.jsx
@@ -1,40 +1,51 @@
-import { usePlayer } from "@/hooks/usePlayer";
-import { parseUrl } from "@/util/videoUtils";
-import React, { useEffect, useRef, useState } from "react";
-import Youtube from "react-youtube";
+import React, { useState } from "react";
+import YouTube from "react-youtube";
+import useSocket from "@/hooks/useSocket";
+import usePlayer from "@/hooks/usePlayer";
 
-const VideoComponent = (props) => {
-  const opts = {
-    height: "390",
-    width: "640",
+const VideoComponent = () => {
+  const [videoIdValue, setVideoIdValue] = useState("r4Pq5lygij8");
+  const { socket, on, emit } = useSocket("http://192.168.0.28:8000");
+  const { player, setPlayer, onPlayerStateChange } = usePlayer(
+    socket,
+    on,
+    emit
+  );
+
+  const onPlayerReady = (event) => {
+    const player = event.target;
+    player.pauseVideo();
+    setPlayer(player);
+  };
+
+  const handleVideoIdChange = (e) => {
+    setVideoIdValue(e.target.value);
+  };
+
+  const options = {
+    height: "500",
+    width: "700",
     playerVars: {
-      autoplay: 1,
+      autoplay: 0,
+      rel: 0,
+      modestbranding: 1,
+      showInfo: 0,
     },
   };
 
-  const {
-    url,
-    playerRef,
-    onStateChange,
-    onReady,
-    handlePlaybackRateChange,
-    playerInfo,
-  } = props;
-
   return (
     <div>
-      <h3>{`currentTime: ${playerInfo.currentTime}`}</h3>
-      <h3>{`playbackRate: ${playerInfo.playbackRate}`}</h3>
-      <h3>{`videoUrl: ${playerInfo.videoUrl}`}</h3>
-      <h3>{`volume: ${playerInfo.volume}`}</h3>
-      <h3>{`playerState: ${playerInfo.playerState}`}</h3>
-      <Youtube
-        ref={playerRef}
-        videoId={parseUrl(url)}
-        opts={opts}
-        onStateChange={onStateChange}
-       // onPlaybackRateChange={handlePlaybackRateChange}
-        onReady={onReady}
+      <YouTube
+        videoId={videoIdValue}
+        opts={options}
+        onReady={onPlayerReady}
+        onStateChange={onPlayerStateChange}
+      />
+      <input
+        id="videoId"
+        type="text"
+        value={videoIdValue}
+        onChange={handleVideoIdChange}
       />
     </div>
   );
--- fe/src/pages/index.js
+++ fe/src/pages/index.js
@@ -3,8 +3,6 @@ import useSocket from "@/hooks/useSocket";
 import { useEffect, useRef, useState } from "react";
 import VideoComponent from "@/components/VideoComponent";
 import { usePlayer } from "@/hooks/usePlayer";
-import VideoComponent3 from "@/components/VideoComponent2";
-
 export default function Home() {
   //const { socket, on, emit } = useSocket("http://localhost:8000");
 
@@ -13,44 +11,10 @@ export default function Home() {
     "https://www.youtube.com/watch?v=r4Pq5lygij8"
   );
 
-  // const onPlayerStateChange = (data) => {
-  //   emit("playerStateChange", {
-  //     playerState: data.playerState,
-  //     currentTime: data.currentTime,
-  //   });
-  // };
-
-  // const onReady = (event) => {
-  //   event.target.pauseVideo();
-  // };
-
-  // const { playerRef, seekTo, onStateChange, play, pause, playerInfo } =
-  //   usePlayer(onPlayerStateChange);
-
-  // useEffect(() => {
-  //   on("handlePlayerStateChange", (data) => {
-  //     const parsedData = JSON.parse(data);
-  //     if (parsedData.playerState == "1") {
-  //       play();
-  //     } else if (parsedData.playerState == "2") {
-  //       pause();
-  //     }
-  //     seekTo(parsedData.seekTo);
-  //   });
-  // }, [on]);
-
   return (
     <>
       <main className={styles.main}>
-        {/* <VideoComponent
-          onStateChange={onStateChange}
-          onReady={onReady}
-          playerRef={playerRef}
-          playerInfo={playerInfo}
-          url={videoUrl}
-        /> */}
-
-        <VideoComponent3 />
+        <VideoComponent />
         <input ref={inputRef} />
         <button onClick={() => setVideoUrl(inputRef.current.value)}>
           Load Video
